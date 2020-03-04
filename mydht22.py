c2511896ba2d1dbead2cad50c14865eb970aac14
Courtney Sampson <36641312+court-samp@users.noreply.github.com>
2020-03-04 09:55:33 -0500

    Add files via upload

---
+++ mydht22.py
@@ -0,0 +1,14 @@
+import Adafruit_DHT
+import time
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+while True:
+	humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+	if humidity is not None and temperature is not None:
+		temperature = temperature * 1.8 +32
+		print("Temp={0:0.1f}F Humidity={1:0.1f}%".format(temperature, humidity))
+	else:
+		print("Sensor failure. Check wiring.");
+	time.sleep(3);
